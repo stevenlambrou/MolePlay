23a7555b5871f4ab2c8dbd2bc7f0b210107ec5e6
Connor Demille <subtlepseudonym@gmail.com>
2015-11-22 17:48:52 -0500

    fixed bugs in devsitemap.jade

--- WebApp/routes/index.js
+++ WebApp/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
-var router = express.Router();
+var jade    = require('jade');
+var router  = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Index' });
+    res.render('devsitemap', { title: 'Navigation'});
+    // res.render('index', { title: 'Index' });
 });
 
 module.exports = router;
