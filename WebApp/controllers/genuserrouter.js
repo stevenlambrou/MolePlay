aa4a8490efbf62401875212ca2de2fc1c19142c2
Connor Demille <subtlepseudonym@gmail.com>
2015-12-01 14:05:06 -0500

    fixed genuserrouter.js routes

--- WebApp/controllers/genuserrouter.js
+++ WebApp/controllers/genuserrouter.js
@@ -1,36 +1,35 @@
-
 var express = require('express');
 var router  = express.Router();
-var path = require('path');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Index' });
+});
+
+router.get('/playwithmolecules', function(req, res, next) {
+  // TODO: create .post('/playwithmolecules') to service search fn
+  res.render('playWithMolecules', { title: 'Play With Molecules' });
+});
+
+router.get('/createaccount', function(req, res, next) {
+  // TODO: need something like .post('createaccount') for new account forms
+  res.render('createAccount', { title: 'Create An Account' });
+});
+
+router.get('/login', function(req, res, next) {
+  // TODO: .post('/login') or .post('/auth') for login credentials
+  res.render('logIn', { title: 'Log In' });
+});
+
+router.get('/devsitemap', function(req, res, next) {
+  res.render('devsitemap', { title: 'devsitemap' });
+});
 
 
-function homepage(req, res){
-	res.render('index');
-}
-function playWithMolecules(req, res){
-	res.render('playwithmolecules');
-}
-function createAccount(req,res){
-	res.render('createaccount');
-}
-
-
-router.get('/', homepage);
-router.get('/createAccount', createAccount);
-
+/* POST routes */
+router.post('/playwithmolecules', function(req, res, next) {
+  res.render('playWithMolecules', { title: 'Play With Molecules' });
+  res.send(req.search);
+});
 
 module.exports = router;
-// router.get('/playwithmolecules', function(req, res, next) {
-//   // TODO: create .post('/playwithmolecules') to service search fn
-//   res.render('playWithMolecules', { title: 'Play With Molecules' });
-// });
-
-// router.get('/createaccount', function(req, res, next) {
-//   // TODO: need something like .post('createaccount') for new account forms
-//   res.render('createAccount', { title: 'Create An Account' });
-// });
-
-// router.get('/login', function(req, res, next) {
-//   // TODO: .post('/login') or .post('/auth') for login credentials
-//   res.render('logIn', { title: 'Log In' });
-// });
