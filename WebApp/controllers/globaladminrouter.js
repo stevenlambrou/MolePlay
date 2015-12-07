fb0271b64017eec1cc796dd4f2376363442e8084
Ethan Smith <element.above@gmail.com>
2015-12-07 15:43:23 -0500

    changed to link to correct adminportal. fixed css linking

--- WebApp/controllers/globaladminrouter.js
+++ WebApp/controllers/globaladminrouter.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router  = express.Router();
 
 function homepage(req, res, next) {
-  res.render('adminLayout');
+  res.render('adminPortal');
 }
 
 /* GET routes */
