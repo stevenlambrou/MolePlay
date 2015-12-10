47639cc5620e820ecb8a67cee506ed00db6a38ef
ethanmsmith <ethansmith@umass.edu>
2015-12-10 14:34:41 -0500

    create playlist mostly functional. portals being fixed

--- WebApp/public/js/playlists.js
+++ WebApp/public/js/playlists.js
@@ -5,47 +5,20 @@ function populateMolecules() {
 
 
     for(var i = 0; i < mols; i++) {
-      var btn = $('<a>');
-      btn.text(data.molecules[i].name);
-      btn.attr("draggable","true");
-      btn.attr("ondragstart","drag(event)");
-      btn.attr("id",data.molecules[i].name);
-      btn.attr("class","btn-default btn");
-      playlist.append(btn);
+      var div = $('<div>');
+      var time = $('<input>');
+      div.text(data.molecules[i].name);
+      div.attr("draggable","true");
+      div.attr("ondragstart","drag(event)");
+      div.attr("id",data.molecules[i].name);
+      div.attr("class","btn-default btn");
+      div.attr("author",data.molecules[i].author);
+      time.attr("name","time");
+      time.attr("placeholder","Sec.");
+      time.attr("size","5");
+      div.append("<br>");
+      div.append(time);
+      playlist.append(div);
     }
-  });
-}
-
-function createPlaylist() {
-  var playlist = { 'molecules' : [
-
-  ]};
-  var name = $("#name").val();
-   $('#playlist').children().each(function () {
-    console.log($(this).text()); //log every element found to console output
-    var text = $(this).text();
-    var mol = {text};
-    playlist.molecules.push(mol);
-  });
-  console.log(playlist);
-  if(name === "") {
-    alert("Please give the playlist a name.");
-    return;
-  }
-  writeToFile(playlist);
-}
-
-function writeToFile(jsonObject) {
-  $.getJSON("/playlists.json", function(data) {
-    var tmp = data;
-    tmp.playlists.push(jsonObject);
-    $.ajax({
-      type : "POST",
-      url : "playlists.json",
-      dataType : 'json',
-      data : {
-          tmp /* convert here only */
-      }
-    });
   });
 }
